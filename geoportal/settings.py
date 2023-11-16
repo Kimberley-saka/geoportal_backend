36ddb84f6346b362a685575d680a2a0c3202f519
Kimberley saka <kimberleysaka@gmail.com>
2023-11-16 10:36:43 +0200

    added simplejwt

--- geoportal/settings.py
+++ geoportal/settings.py
@@ -113,6 +113,13 @@ PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.ScryptPasswordHasher",
 ]
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
