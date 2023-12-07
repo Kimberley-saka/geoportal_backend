3d9ffad11b32738ccf9f8d4d2bc8598eea24f0f5
Kimberley-saka <kimberleysaka@gmail.com>
2023-12-07 15:25:39 +0200

    added reset password view

--- users/api/urls.py
+++ users/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import MyTokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import GetUserView, CreateUserView
+from .views import GetUserView, CreateUserView, ResetPasswordView
 
 
 urlpatterns =[
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('user/', GetUserView.as_view()),
-    path('create/', CreateUserView.as_view())
+    path('create/', CreateUserView.as_view()),
+    path('password_reset/', ResetPasswordView.as_view(), name='password_reset'),
 ]
