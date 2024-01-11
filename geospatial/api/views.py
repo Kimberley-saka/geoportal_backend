2eef86d14b33adc3e389428e8112cf5165ec37c0
Kimberley-saka <kimberleysaka@gmail.com>
2024-01-11 16:45:25 +0200

    Refactor getGeoData API to include search functionality

--- geospatial/api/views.py
+++ geospatial/api/views.py
@@ -1,6 +1,39 @@
 from rest_framework.decorators import APIView
+import requests
+import json
+from rest_framework import status
 from rest_framework.response import Response
 
 class getGeoData(APIView):
-    def get(self, request):
-        return Response('some data')
+    def get(self, request, *args, **kwargs):
+        search_term = request.data.get('searchTerm')
+
+        geoserver_url = 'url'
+        response = requests.get(geoserver_url)
+        shapefile_info = response.json()
+
+        for feature_type in shapefile_info['featureTypes']:
+            if feature_type['name'] == search_term:
+                download_url = feature_type['links'][1]['href']
+                break
+        
+        else:
+            return Response({'detail: data not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        shapefile_data = requests.get(download_url).content
+
+
+        geojson_response = {
+            "type": "FeatureCollection",
+            "features": [
+                {
+                    "type": "Feature",
+                    "geometry": json.loads(shapefile_data),
+                    "properties": {},
+                }
+            ]
+        }
+
+        return Response(geojson_response)
+
+    
