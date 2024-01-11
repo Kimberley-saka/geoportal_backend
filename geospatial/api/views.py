d4ecd9a1474c047a4355ac68c513df15ede62126
Kimberley-saka <kimberleysaka@gmail.com>
2024-01-11 17:22:20 +0200

    Update geoserver URL and handle response status code

--- geospatial/api/views.py
+++ geospatial/api/views.py
@@ -8,17 +8,19 @@ class getGeoData(APIView):
     def get(self, request, *args, **kwargs):
         search_term = request.data.get('searchTerm')
 
-        geoserver_url = 'localhost:8080/geoserver/rest/workspaces/shapefiles/datastores/shapefiles/featuretypes'
+        geoserver_url = 'http://localhost:8090/geoserver/rest/workspaces/shapefiles/datastores/geoportal/featuretypes'
         response = requests.get(geoserver_url)
-        shapefile_info = response.json()
+        if response.status_code == 200:
+            shapefile_info = response.json()
+        
+        else:
+            return Response({'detail: data not found'}, status=status.HTTP_404_NOT_FOUND)
 
         for feature_type in shapefile_info['featureTypes']:
             if feature_type['name'] == search_term:
                 download_url = feature_type['links'][1]['href']
                 break
         
-        else:
-            return Response({'detail: data not found'}, status=status.HTTP_404_NOT_FOUND)
         
         shapefile_data = requests.get(download_url).content
 
