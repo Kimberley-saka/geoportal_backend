bff3aafe989ba267a4987a56993e3275db36e3b1
Kimberley-saka <kimberlyesaka@gmail.com>
2024-06-13 19:36:16 +0200

    added user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    User model
+    """
+    firstname = models.CharField(max_length=250, null=False)
+    lastname = models.CharField(max_length=250, null=False)
+    email = models.EmailField(max_length=250, null=False, unique=True)
+    password = models.CharField(max_length=250, null=False)
+
+
+    REQUIRED_FIELDS = ['email', 'password']
+
+    class Meta:
+        """meta data"""
+
+        db_table = 'users'
+
+
+
